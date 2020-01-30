7b1cf1fb2fd26aa749dc68badc52e956759be1b9
Nuno Morais <nram@nuno-morais.eu>
2020-01-30 17:41:10 +0000

    Implement Stats controller

---
+++ src/stats.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class StatsService {
+  private stats = {};
+  private readonly defaultStats = {
+    created: 0,
+    updated: 0,
+    deleted: 0,
+  };
+
+  public get(userId: string) {
+    return this.stats[userId] || this.defaultStats;
+  }
+
+  public increaseCreated(userId: string) {
+    const current = this.get(userId);
+    this.stats[userId] = { ...current, created: current.created + 1 };
+  }
+
+  public increaseUpdate(userId: string) {
+    const current = this.get(userId);
+    this.stats[userId] = { ...current, updated: current.updated + 1 };
+  }
+
+  public increaseDelete(userId: string) {
+    const current = this.get(userId);
+    this.stats[userId] = { ...current, deleted: current.deleted + 1 };
+  }
+}
